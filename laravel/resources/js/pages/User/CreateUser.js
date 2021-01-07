7fb37b0d4a276c1e848d2670e2fd9ecb03dc306c
nyanmyohtet <scm.nyanmyohtet@gmail.com>
2021-01-08 02:11:39 +0630

    Clean CreateUser Page

--- laravel/resources/js/pages/User/CreateUser.js
+++ laravel/resources/js/pages/User/CreateUser.js
@@ -8,48 +8,16 @@ export default class CreateUser extends Component {
         super(props);
 
         this.state = {
-            loading: false,
-            validated: false,
             errors: {},
-            name: "user03",
-            email: "user03@example.com",
-            password: "user03",
-            cPassword: "user03",
-            type: 2,
-            phone: "959700808022",
-            dateOfBirth: "2021-01-06",
-            address: "Yangon",
             selectedFile: null,
             imagePreviewUrl: null
         };
 
-        this.handleAllInputs = this.handleAllInputs.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleReset = this.handleReset.bind(this);
         this.handleProfile = this.handleProfile.bind(this);
     }
 
-    handleAllInputs(e) {
-        let { name, value } = e.target;
-        if (name === "type") {
-            value = parseInt(value);
-        }
-
-        if (name === "phone") {
-            const phoneNoPattern = /^\d{12}$/;
-            if (value.match(phoneNoPattern)) {
-                console.warn("valid phone no");
-            } else {
-                console.warn("invalid phone no format");
-            }
-        }
-        console.log(name, " : ", value);
-        this.setState({ [name]: value });
-    }
-
     handleSubmit(values) {
-        console.log("create user form submit...");
-
         const formData = new FormData();
         formData.append("name", values.name);
         formData.append("email", values.email);
@@ -92,55 +60,7 @@ export default class CreateUser extends Component {
             });
     }
 
-    handleReset(e) {
-        e.preventDefault();
-        this.setState({
-            name: "",
-            email: "",
-            password: "",
-            cPassword: "",
-            type: "",
-            phone: "",
-            dateOfBirth: "",
-            address: ""
-        });
-    }
-
-    // handleProfile(event) {
-    //     event.preventDefault();
-    //     console.log(event);
-    //     console.log(event.target.files);
-    //     this.setState(
-    //         {
-    //             selectedFile: event.target.files[0]
-    //         },
-    //         () => console.log(this.state.selectedFile)
-    //     );
-
-    //     let reader = new FileReader();
-    //     reader.onloadend = () => {
-    //         this.setState(
-    //             {
-    //                 imagePreviewUrl: reader.result
-    //             },
-    //             console.log(this.state.imagePreviewUrl)
-    //         );
-    //     };
-    //     reader.readAsDataURL(event.target.files[0]);
-    // }
-
     handleProfile(event) {
-        console.log(event);
-
-        // const files = Array.from(event.target.files);
-        // // this.setState({ uploading: true });
-
-        // const formData = new FormData();
-        // debugger;
-
-        // files.forEach((file, i) => {
-        //     formData.append(i, file);
-        // });
         this.setState(
             {
                 selectedFile: event.target.files[0]
@@ -150,24 +70,14 @@ export default class CreateUser extends Component {
     }
 
     render() {
-        const {
-            name,
-            email,
-            password,
-            cPassword,
-            type,
-            phone,
-            dateOfBirth,
-            address
-        } = this.state;
         return (
-            <Fragment>
+            <div className="form-container">
                 <h2>Create User</h2>
                 <UserCreateForm
                     handleSubmit={this.handleSubmit}
                     handleProfile={this.handleProfile}
                 />
-            </Fragment>
+            </div>
         );
     }
 }
